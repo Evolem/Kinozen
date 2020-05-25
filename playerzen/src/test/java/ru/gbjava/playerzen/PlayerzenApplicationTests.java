6e9705f811f070fd1e1e614b82a5ddffbac8b19c
Milkky <Milkky598@yandex.ru>
2020-05-25 18:56:25 +0500

    Изменение структуры проекта - добавлены multi-model

---
+++ playerzen/src/test/java/ru/gbjava/playerzen/PlayerzenApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gbjava.playerzen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerzenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
