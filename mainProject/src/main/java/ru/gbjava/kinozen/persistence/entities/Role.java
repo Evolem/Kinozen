215b3faef4412c723ad44f5c59bda2c9b20de100
Vladimir Iakimov <yakimovvn@bk.ru>
2020-05-26 21:43:09 +0300

    Addad entity for security

---
+++ mainProject/src/main/java/ru/gbjava/kinozen/persistence/entities/Role.java
@@ -0,0 +1,37 @@
+package ru.gbjava.kinozen.persistence.entities;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+/**
+ * Created by IntelliJ Idea.
+ * User: Якимов В.Н.
+ * E-mail: yakimovvn@bk.ru
+ */
+
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "tbl_role")
+public class Role {
+
+    @Id
+    @Column(name = "id_user")
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @Column(name = "name_role")
+    private String role;
+
+}
--- mainProject/src/main/java/ru/gbjava/kinozen/persistence/entities/User.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/persistence/entities/User.java
@@ -1,4 +1,50 @@
 package ru.gbjava.kinozen.persistence.entities;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "tbl_user")
 public class User {
+
+
+    @Id
+    @Column(name = "id_user")
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @Column(name = "login_user")
+    private String login;
+
+
+    @Column(name = "password_user")
+    private String password;
+
+    @Column(name = "name_user")
+    private String name;
+
+    @Column(name = "email_user")
+    private String email;
+
+
+    @ManyToMany
+    @JoinTable(name="tbl_role_user", joinColumns=
+    @JoinColumn(name="id_user", referencedColumnName="id_user"), inverseJoinColumns=
+    @JoinColumn(name="id_role", referencedColumnName="id_role"))
+    private List<Role> roles = new ArrayList<>();
+
 }
