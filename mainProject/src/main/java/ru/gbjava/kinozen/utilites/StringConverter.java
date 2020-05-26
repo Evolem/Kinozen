b878c32ae5a97325794eeb5fad9f5d7aadcb294a
kirilllebedev <Lebedev.a.l@mail.ru>
2020-05-26 22:04:22 +0300

    Add utility to convert cyrillic strings to latin

---
+++ mainProject/src/main/java/ru/gbjava/kinozen/utilites/StringConverter.java
@@ -0,0 +1,13 @@
+package ru.gbjava.kinozen.utilites;
+
+import com.ibm.icu.text.Transliterator;
+
+public class StringConverter {
+
+    private static final String CYRILLIC_TO_LATIN = "Russian-Latin/BGN";
+
+    public static String cyrillicToLatin(String cyrillic) {
+        Transliterator toLatin =  Transliterator.getInstance(CYRILLIC_TO_LATIN);
+        return toLatin.transliterate(cyrillic).toLowerCase();
+    }
+}
