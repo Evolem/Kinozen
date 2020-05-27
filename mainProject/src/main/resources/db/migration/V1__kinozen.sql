9489bb89724d57aadc7af8714cd7d153cfacdea7
Vladimir Iakimov <yakimovvn@bk.ru>
2020-05-27 11:49:34 +0300

    Mave back

--- mainProject/src/main/resources/db/migration/V1__kinozen.sql
+++ mainProject/src/main/resources/db/migration/V1__kinozen.sql
@@ -222,3 +222,6 @@ create table if not exists tbl_director_media
 
 alter table tbl_director_media owner to postgres;
 
+insert into tbl_role(id_role, name_role) values (1, 'ROLE_ADMIN');
+insert into tbl_user(id_user, login_user, password_user, name_user) values (1, 'admin', '$2a$10$5rAOMKmVsh9.NlzXTLLbq.XwouGdg3dwohvb5/HDn692YfdrLthO2', 'vladimir');
+insert into tbl_role_user (id_role, id_user) VALUES (1,1);
