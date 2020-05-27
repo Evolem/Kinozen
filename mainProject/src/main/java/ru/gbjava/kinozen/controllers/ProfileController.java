6cb948674874677b0d620ccdcee7841ca1132a2f
kirilllebedev <Lebedev.a.l@mail.ru>
2020-05-28 01:43:58 +0300

    Add profile page and ability to change fields of user

--- mainProject/src/main/java/ru/gbjava/kinozen/controllers/ProfileController.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/controllers/ProfileController.java
@@ -1,15 +1,50 @@
 package ru.gbjava.kinozen.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.gbjava.kinozen.persistence.entities.User;
+import ru.gbjava.kinozen.services.UserService;
+import ru.gbjava.kinozen.services.pojo.UserP;
+
+import javax.validation.Valid;
+import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/profile")
 public class ProfileController {
+    private final UserService userService;
 
     @GetMapping
-    public String profilePage(){
+    public String profilePage(Principal principal, Model model, UserP userP){
+        User user = userService.findByLogin(principal.getName());
+        userP.setLogin(user.getLogin());
+        userP.setEmail(user.getEmail());
+        userP.setName(user.getName());
+        model.addAttribute("userP", userP);
         return "profile";
+    }
+
+    @PostMapping("/change")
+    public String changeProfile(Principal principal, @Valid UserP userP, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "profile";
+        }
+        User user = userService.findByLogin(principal.getName());
+        if (!BCrypt.checkpw(userP.getPassword(), user.getPassword())) {
+            bindingResult.rejectValue("password", "Error", "Некорректный пароль");
+            return "profile";
+        }
+
+        user.setEmail(userP.getEmail());
+        user.setName(userP.getName());
+        userService.save(user);
+        return "redirect:/profile";
     }
 }
--- mainProject/src/main/java/ru/gbjava/kinozen/services/pojo/UserP.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/services/pojo/UserP.java
@@ -1,4 +1,32 @@
 package ru.gbjava.kinozen.services.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserP {
+
+    private String login;
+
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String name;
+
+    @NotNull
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotEmpty
+    private String password;
 }
