0a9d3c4b11cb2a05b06c69a32d60c696413fca2c
lebedev Kirill <42681868+Lal007@users.noreply.github.com>
2020-05-28 16:21:56 +0300

    Profile page (#12)
    * Add profile page and ability to change fields of user
    
    * Add change password page and fixes

--- mainProject/src/main/java/ru/gbjava/kinozen/controllers/ProfileController.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/controllers/ProfileController.java
@@ -1,15 +1,78 @@
 package ru.gbjava.kinozen.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.gbjava.kinozen.persistence.entities.User;
+import ru.gbjava.kinozen.services.UserService;
+import ru.gbjava.kinozen.services.pojo.UserPojo;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.Objects;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/profile")
 public class ProfileController {
+    private final UserService userService;
 
     @GetMapping
-    public String profilePage(){
+    public String profilePage(Principal principal, Model model, UserPojo userPojo){
+        User user = userService.findByLogin(principal.getName());
+        userPojo.setEmail(user.getEmail());
+        userPojo.setName(user.getName());
+        model.addAttribute("userPojo", userPojo);
         return "profile";
+    }
+
+    @GetMapping("/password")
+    public String profilePassPage(UserPojo userPojo){
+        return "profilePass";
+    }
+
+    @PostMapping("/change")
+    public String changeProfile(Principal principal, @Valid UserPojo userPojo, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "profile";
+        }
+        User user = userService.findByLogin(principal.getName());
+        if (!BCrypt.checkpw(userPojo.getPassword(), user.getPassword())) {
+            bindingResult.rejectValue("password", "Error", "Некорректный пароль");
+            return "profile";
+        }
+
+        user.setEmail(userPojo.getEmail());
+        user.setName(userPojo.getName());
+        userService.save(user);
+        return "redirect:/profile";
+    }
+
+    @PostMapping("/newPass")
+    public String changePassword(Principal principal, UserPojo userPojo, BindingResult bindingResult) {
+        User user = userService.findByLogin(principal.getName());
+
+        if (userPojo.getNewPassword1().isEmpty() && userPojo.getNewPassword2().isEmpty()) {
+            bindingResult.rejectValue("newPassword1", "Error", "Пароли не введены");
+            return "profilePass";
+        }
+        if (!Objects.equals(userPojo.getNewPassword1(), userPojo.getNewPassword2())) {
+            bindingResult.rejectValue("newPassword1", "Error", "Пароли не совпадают");
+            return "profilePass";
+        }
+        if (!BCrypt.checkpw(userPojo.getPassword(), user.getPassword())) {
+            bindingResult.rejectValue("password", "Error", "Некорректный пароль");
+            return "profilePass";
+        }
+
+        user.setPassword(new BCryptPasswordEncoder().encode(userPojo.getNewPassword1()));
+        userService.save(user);
+        return "redirect:/profile";
     }
 }
--- mainProject/src/main/java/ru/gbjava/kinozen/services/pojo/UserPojo.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/services/pojo/UserPojo.java
@@ -1,4 +1,36 @@
 package ru.gbjava.kinozen.services.pojo;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserPojo {
+
+    private String login;
+
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String name;
+
+    @NotNull
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    private String newPassword1;
+    private String newPassword2;
 }
