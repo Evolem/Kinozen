0dac99ac74d892537e447709e90eb378d9882b30
svyatoslavn1000 <s-e-ny@yandex.ru>
2020-05-29 13:49:36 +0300

    Добавил страницу редактирования режиссера (director)

--- mainProject/src/main/java/ru/gbjava/kinozen/controllers/DirectorController.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/controllers/DirectorController.java
@@ -16,7 +16,6 @@ import java.util.List;
 public class DirectorController {
     @Autowired
     private DirectorService directorService;
-    private DirectorService mediaService;
 
     @GetMapping("/{id}")
     public String showDirector(Model model, @PathVariable Long id) {
@@ -28,14 +27,12 @@ public class DirectorController {
     @GetMapping("/edit/{id}")
     public String editProductForm(Model model, @PathVariable Long id) {
         Director director = directorService.findById(id);
-        List<Media> media = mediaService.getAll();
-        model.addAttribute("product", director);
-        model.addAttribute("categories", media);
-        return "edit_director";
+        model.addAttribute("director", director);
+        return "director_edit";
     }
 
     @PostMapping("/edit")
-    public String saveProduct(@ModelAttribute(name = "product") Director director) {
+    public String saveProduct(@ModelAttribute(name = "director") Director director) {
         directorService.save(director);
         return "redirect:/";
     }
