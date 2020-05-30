be5913b604c06dcc43918c108af09b46e4dc13d6
leonid <leo_lis@mail.ru>
2020-05-30 23:35:18 +0300

    actor_page. страница актера. добавлен pojo

---
+++ mainProject/src/main/java/ru/gbjava/kinozen/services/pojo/ActorPojo.java
@@ -0,0 +1,12 @@
+package ru.gbjava.kinozen.services.pojo;
+
+import lombok.Data;
+
+@Data
+public class ActorPojo {
+
+    private String firstName;
+    private String lastName;
+    private String description;
+
+}
