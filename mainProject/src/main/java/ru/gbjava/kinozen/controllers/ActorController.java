0577db8729a0c15b5dc74f2c2a4ef4ca18dcd7c2
LeonidLisin <42032453+LeonidLisin@users.noreply.github.com>
2020-06-01 11:05:10 +0300

    Actor page (#20)
    * actor_page. страница актера. добавлен репозиторий актера
    
    * actor_page. страница актера. добавлен pojo
    
    * actor_page. страница актера. добавлена сущность актера
    
    * actor_page. страница актера. добавлен сервис актера
    
    * actor_page. страница актера. аннотация @Data заменена на две: @Getter + @Setter, иначе не работало получение списка медиа для актера (бесконечная рекурсия, stack overflow)
    
    * actor_page. страница актера. добавлен контроллер актера
    
    * actor_page. страница актера
    
    * actor_page. страница актера. добавлен метод получения списка медиа актера
    
    * actor_page. страница актера. теперь список медиа актера выводится ссылками на эти медиа
    
    * actor_page. страница актера. переделал под новую бд
    
    * actor_page. страница актера. добавил поле для картинок
    
    * actor_page. страница актера

---
+++ mainProject/src/main/java/ru/gbjava/kinozen/controllers/ActorController.java
@@ -0,0 +1,41 @@
+package ru.gbjava.kinozen.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.gbjava.kinozen.services.ActorService;
+import ru.gbjava.kinozen.services.pojo.ActorPojo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/actor")
+@RequiredArgsConstructor
+public class ActorController {
+
+    private final ActorService actorService;
+
+    @GetMapping("/{id}")
+    public String getActorById(Model model, @PathVariable int id){
+        model.addAttribute("actor", actorService.getActorById(id));
+        return "actor";
+    }
+
+    @PostMapping /* /actor - endpoint для добавления новго актера */
+    public void addActor(ActorPojo actorPojo, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        actorService.save(actorPojo);
+        response.sendRedirect(request.getHeader("referer"));
+    }
+
+    @GetMapping
+    public String addActor(){
+        return "addActor";
+    }
+
+}
