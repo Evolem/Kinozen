1cef7f38923470f7772be0a9f2a506e48eaa618d
kirilllebedev <Lebedev.a.l@mail.ru>
2020-06-02 16:12:37 +0300

    fix transliterator

--- mainProject/src/main/java/ru/gbjava/kinozen/utilites/StringConverter.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/utilites/StringConverter.java
@@ -4,10 +4,12 @@ import com.ibm.icu.text.Transliterator;
 
 public class StringConverter {
 
-    private static final String CYRILLIC_TO_LATIN = "Russian-Latin/BGN";
+    private static final String CYRILLIC_TO_LATIN = "Russian-Latin/BGN; Latin-ASCII";
 
     public static String cyrillicToLatin(String cyrillic) {
         Transliterator toLatin = Transliterator.getInstance(CYRILLIC_TO_LATIN);
-        return toLatin.transliterate(cyrillic).toLowerCase().trim().replaceAll("\\s+", "-");
+        return toLatin.transliterate(cyrillic).toLowerCase().trim()
+                .replaceAll("\\s+", "-")
+                .replaceAll("[^a-z0-9_-]", "");
     }
 }
