adf928bb1f77ad638fabc882c28941bbd274e539
pavlo <pavlov-sta@mail.ru>
2020-06-03 22:14:54 +0300

    T-F5_Front-end_content. Добави на главну страницу выгрузку контента. Добавил страницу контента. Вынес слайдер в блоки. Удалил лишьнее.

--- mainProject/src/main/java/ru/gbjava/kinozen/controllers/HomeController.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/controllers/HomeController.java
@@ -5,13 +5,21 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.gbjava.kinozen.dto.ContentDto;
+import ru.gbjava.kinozen.services.ContentService;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+    private final ContentService contentService;
+
     @GetMapping
     public String index(Model model){
+        List<ContentDto> dtoList = contentService.getAllContent();
+        model.addAttribute("contentList", dtoList);
         return "index";
     }
 }
