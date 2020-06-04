8e1e1fa07e04b0a158ed1695c1dc1b194cb24cfd
Milkky <Milkky598@yandex.ru>
2020-06-04 20:19:57 +0500

    T-C7 Проверка категории на плеере

--- playerzen/src/main/resources/db/migration/V1__playerzen.sql
+++ playerzen/src/main/resources/db/migration/V1__playerzen.sql
@@ -10,3 +10,5 @@ create table if not exists tbl_media_file
 create unique index if not exists tbl_media_file_id_media_file_uindex
     on tbl_media_file (id_media_file);
 
+insert into tbl_media_file (uuid_media, name_media_file)
+values ('1740acb5-a8c6-43f8-b8e1-faa74c92ea4a', 'love.death.robot.S01.2')
