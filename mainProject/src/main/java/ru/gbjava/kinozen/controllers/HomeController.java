060639c956d6a0d75a00b2c3dc0d016e55479571
Pavlov Stanislav <45894629+pavlov-sta@users.noreply.github.com>
2020-06-04 23:53:01 +0300

    T-F5_Front-end_content Загрузил страницу сериалов. (#47)
    T-F4_Front-end_registration Загрузил страницу регестрации.
    T-F3_Front-end_content_page Загрузил страницу сериала.
    T-F3_Front-end_login Загрузил страницу входа.
    
    Co-authored-by: Evgeny E <50954652+8Seph@users.noreply.github.com>

--- mainProject/src/main/java/ru/gbjava/kinozen/controllers/HomeController.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/controllers/HomeController.java
@@ -1,17 +1,22 @@
 package ru.gbjava.kinozen.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.gbjava.kinozen.dto.ContentDto;
+import ru.gbjava.kinozen.dto.mappers.ContentMapper;
+import ru.gbjava.kinozen.services.facade.ContentFacade;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+    private final ContentFacade contentFacade;
     @GetMapping
     public String index(Model model){
+        Iterable<ContentDto> dtoList = ContentMapper.INSTANCE.toDtoList(contentFacade.findAllContent());
+        model.addAttribute("contentList", dtoList);
         return "index";
     }
 }
