60cb0174d8312e9cd6b7cf69d161a84a95e497aa
Евгений <evem-w@outlook.com>
2020-06-04 12:11:13 +0300

    T-REF2_Refactoring_data_type Отключил весь код в DirectorController

--- mainProject/src/main/java/ru/gbjava/kinozen/controllers/DirectorController.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/controllers/DirectorController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.gbjava.kinozen.dto.DirectorDto;
 import ru.gbjava.kinozen.persistence.entities.Director;
 import ru.gbjava.kinozen.services.DirectorService;
 
@@ -18,24 +19,23 @@ public class DirectorController {
 
     //todo переделать!
 
-    @GetMapping("/{id}")
-    public String getDirectorById(Model model, @PathVariable UUID id) {
-        Director director = directorService.findById(id);
-        model.addAttribute("director", director);
-        return "directorPage";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String editProductForm(Model model, @PathVariable UUID id) {
-        Director director = directorService.findById(id);
-        model.addAttribute("director", director);
-        return "directorEdit";
-    }
-
-    @PostMapping("/edit")
-    public String saveDirector(@ModelAttribute(name = "director") Director director) {
-        directorService.save(director);
-        return "redirect:/directorPage/"+ director.getId();
-    }
+//    @GetMapping("/{id}") // поиск по url
+//    public String getDirectorById(Model model, @PathVariable UUID id) {
+//        Director director = directorService.findById(id);
+//        model.addAttribute("director", director);
+//        return "directorPage";
+//    }
+//
+//    @GetMapping("/edit/{id}")
+//    public String editProductForm(Model model, @PathVariable UUID id) {
+//        Director director = directorService.findById(id);
+//        model.addAttribute("director", director);
+//        return "directorEdit";
+//    }
+//
+//    @PostMapping("/edit")
+//    public void add(@ModelAttribute(name = "director") DirectorDto directorDto) {
+//        //todo
+//    }
 
 }
