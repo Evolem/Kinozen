ee309f7ced7e219335733baebedb824474354e44
Евгений <evem-w@outlook.com>
2020-06-05 19:26:01 +0300

    T-C5_Seasons_episodes Добавлены классы Episode и Season

---
+++ mainProject/src/main/java/ru/gbjava/kinozen/persistence/entities/Season.java
@@ -0,0 +1,31 @@
+package ru.gbjava.kinozen.persistence.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_season")
+public class Season {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_season")
+    private UUID id;
+
+    @Column(name = "id_content")
+    @ManyToOne
+    private Content content;
+
+    @Column(name = "number_season")
+    private Integer numberSeason;
+
+    @Column(name = "description_season")
+    private String description;
+}
