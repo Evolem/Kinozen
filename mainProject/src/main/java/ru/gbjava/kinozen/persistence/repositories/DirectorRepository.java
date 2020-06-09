d2ee8c31eeecd0688ff731f837697c45853d2045
Всеволод Григорьев <sevagrigoryev@gmail.com>
2020-06-09 12:32:57 +0300

    T-C10_edit_director Исправил DirectorController, Director, DirectorMapper, DirectorRepository, DirectorService, directorEdit.html, directorPage.html и добавил directorAll.html

--- mainProject/src/main/java/ru/gbjava/kinozen/persistence/repositories/DirectorRepository.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/persistence/repositories/DirectorRepository.java
@@ -3,7 +3,9 @@ package ru.gbjava.kinozen.persistence.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.gbjava.kinozen.persistence.entities.Director;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DirectorRepository extends JpaRepository<Director, UUID> {
+    Optional<Director> findByUrl(String url);
 }
