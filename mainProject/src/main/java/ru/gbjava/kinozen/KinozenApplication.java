e3b6e8052e6c79dba572cd5575b0397e6523d082
Евгений <evem-w@outlook.com>
2020-06-10 14:26:17 +0300

    T-X4_UploadFiles изменены настройки запуска приложения в KinozenApplication

--- mainProject/src/main/java/ru/gbjava/kinozen/KinozenApplication.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/KinozenApplication.java
@@ -1,15 +1,29 @@
 package ru.gbjava.kinozen;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import ru.gbjava.kinozen.configurations.StorageProperties;
+import ru.gbjava.kinozen.services.storage.StorageService;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class KinozenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KinozenApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
