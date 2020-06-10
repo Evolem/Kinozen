0204e969dfc0e57d8224c76bbf4811ae2c3bef86
Evgeny E <50954652+8Seph@users.noreply.github.com>
2020-06-10 16:27:01 +0300

    Функционал загрузки файлов (#56)
    * T-X4_UploadFiles создан соответветствующий интерфейс и сервис
    
    * T-X4_UploadFiles создан и настроен FileUploadController
    
    * T-X4_UploadFiles созданы и настроены классы-исключения
    
    * T-X4_UploadFiles добавлен конфигурационный класс StorageProperties.java
    
    * T-X4_UploadFiles изменены настройки запуска приложения в KinozenApplication
    
    * T-X4_UploadFiles новая зависимость org.springframework.boot:spring-boot-configuration-processor
    
    * T-X4_UploadFiles создана тестовая страница для загрузки
    
    * T-X4_UploadFiles изменены настройки в application.yml
    
    * T-X4_UploadFiles исправлены пути сохранения файлов
    
    * T-X4_UploadFiles исправление загрузки
    
    * T-X4_UploadFiles добавление todo
    
    * T-X4_UploadFiles поднят лимит до 2000MB
    
    * T-X4_UploadFiles исправлен редирект
    
    * T-X4_UploadFiles внедрена утилита для генерации имен + еще несколько фиксов
    
    * T-X4_UploadFiles убрано todo
    
    * T-X4_UploadFiles убрано todo

---
+++ mainProject/src/main/java/ru/gbjava/kinozen/services/storage/FileSystemStorageService.java
@@ -0,0 +1,128 @@
+package ru.gbjava.kinozen.services.storage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import ru.gbjava.kinozen.exceptions.StorageException;
+import ru.gbjava.kinozen.exceptions.StorageFileNotFoundException;
+import ru.gbjava.kinozen.utilites.FileNameGenerator;
+
+import javax.annotation.PostConstruct;
+
+
+@Service
+@RequiredArgsConstructor
+public class FileSystemStorageService implements StorageService{
+
+ //   @Value("${files.storage.video_download}")
+    private Path rootLocation;
+
+//    @Autowired
+//    public FileSystemStorageService(StorageProperties properties) {
+//        this.rootLocation = properties.getLocation();
+//    }
+
+
+    //todo временный хард код, пока не разберусь с ресурсом
+    @PostConstruct
+    private void initPath(){
+        this.rootLocation = Paths.get("C:\\video_download");
+    }
+
+    @Override
+    public String store(MultipartFile file) {
+
+
+        System.out.println(StringUtils.cleanPath(file.getContentType()));
+
+        // Использование утилиты для генерации имен, вместо StringUtils.cleanPath(file.getOriginalFilename());
+        String filename = FileNameGenerator.generate(rootLocation);
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file " + filename);
+            }
+            if (filename.contains("..")) {
+                // This is a security check
+                throw new StorageException(
+                        "Cannot store file with relative path outside current directory "
+                                + filename);
+            }
+
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, this.rootLocation.resolve(filename),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to store file " + filename, e);
+        }
+
+        return filename;
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.rootLocation, 1)
+                    .filter(path -> !path.equals(this.rootLocation))
+                    .map(this.rootLocation::relativize);
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+
+    }
+
+    @Override
+    public Path load(String filename) {
+        return rootLocation.resolve(filename);
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        try {
+            Path file = load(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            }
+            else {
+                throw new StorageFileNotFoundException(
+                        "Could not read file: " + filename);
+
+            }
+        }
+        catch (MalformedURLException e) {
+            throw new StorageFileNotFoundException("Could not read file: " + filename, e);
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(rootLocation.toFile());
+    }
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(rootLocation);
+        }
+        catch (IOException e) {
+            throw new StorageException("Could not initialize storage", e);
+        }
+    }
+}
