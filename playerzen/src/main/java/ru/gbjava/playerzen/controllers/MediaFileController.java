c20c1e6639549c7fddb65a7d126cefa1bb6d06e1
Milkky <Milkky598@yandex.ru>
2020-06-10 19:13:52 +0500

    T-MP3 Загрузка файла.

--- playerzen/src/main/java/ru/gbjava/playerzen/controllers/MediaFileController.java
+++ playerzen/src/main/java/ru/gbjava/playerzen/controllers/MediaFileController.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.gbjava.playerzen.exceptions.EntityNotFoundException;
 import ru.gbjava.playerzen.services.MediaFileService;
 
@@ -25,5 +26,16 @@ public class MediaFileController {
                 .status(HttpStatus.PARTIAL_CONTENT)
                 .contentType(MediaTypeFactory.getMediaType(region.getResource()).orElse(MediaType.APPLICATION_OCTET_STREAM))
                 .body(region);
+    }
+
+    @GetMapping("/")
+    public String page() {
+        return "page";
+    }
+
+    @PostMapping("/")
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        service.uploadMediaFile(file, "f363f0d0-7417-4fba-a00b-f250aecd3958");
+        return "redirect:/video/";
     }
 }
--- playerzen/src/main/java/ru/gbjava/playerzen/services/MediaFileService.java
+++ playerzen/src/main/java/ru/gbjava/playerzen/services/MediaFileService.java
@@ -2,19 +2,24 @@ package ru.gbjava.playerzen.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.UrlResource;
 import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRange;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import ru.gbjava.playerzen.exceptions.EntityNotFoundException;
 import ru.gbjava.playerzen.persistance.entities.MediaFile;
 import ru.gbjava.playerzen.persistance.repositories.MediaFileRepository;
+import ru.gbjava.playerzen.utilites.FileNameGenerator;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,16 +35,43 @@ public class MediaFileService {
     private MediaFile mediaFile;
 
     @Value("${files.storepath.storage}")
-    private String mainPath;
+    private Path MEDIA_STORE;
 
     public MediaFile getMediaFile(String name) throws EntityNotFoundException {
         return  repository.findByMedia(UUID.fromString(name)).orElseThrow(() -> new EntityNotFoundException("File not found: " + name));
     }
 
+    @Transactional
+    public void uploadMediaFile(MultipartFile file, String uuidMedia) {
+
+        if (repository.findByMedia(UUID.fromString(uuidMedia)).isPresent()) {
+            log.error("Content {} already exists", uuidMedia);
+            return;
+        }
+
+        try {
+            if (file.getBytes().length != 0) {
+                String uploadedFileName = FileNameGenerator.generate(MEDIA_STORE) + ".mp4";
+                Path targetLocation = MEDIA_STORE.resolve(uploadedFileName);
+                Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+                MediaFile mediaFile = MediaFile.builder()
+                        .media(UUID.fromString(uuidMedia))
+                        .nameFile(uploadedFileName)
+                        .build();
+                repository.save(mediaFile);
+
+                log.info("File {}, UUID: {} has been successfully uploaded!", uploadedFileName, uuidMedia);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public ResourceRegion getResourceRegion(HttpHeaders headers, String media) throws MalformedURLException, EntityNotFoundException {
         checkMediaFile(media);
 
-        UrlResource resource = new UrlResource(String.format("file:%s/%s.mp4", mainPath, mediaFile.getNameFile()));
+        UrlResource resource = new UrlResource(String.format("file:%s/%s", MEDIA_STORE, mediaFile.getNameFile()));
 
         return resourceRegion(resource, headers);
     }
