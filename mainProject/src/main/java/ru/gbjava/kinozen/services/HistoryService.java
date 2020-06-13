7e4136bf0e1b9451080a593f04dfd93e82173341
leonid <leo_lis@mail.ru>
2020-06-13 22:06:25 +0300

    T-P7_views_history Добавил сохранение в бд и сортировку истории по дате

--- mainProject/src/main/java/ru/gbjava/kinozen/services/HistoryService.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/services/HistoryService.java
@@ -6,6 +6,7 @@ import ru.gbjava.kinozen.persistence.entities.History;
 import ru.gbjava.kinozen.persistence.repositories.HistoryRepository;
 
 import javax.transaction.Transactional;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,13 +15,28 @@ import java.util.UUID;
 public class HistoryService {
 
     private final HistoryRepository historyRepository;
+    private final ContentService contentService;
 
     public List<History> findHistoryByUserId(UUID id){
-        return historyRepository.findAllByIdUser(id).orElseThrow(() -> new RuntimeException("Bad user id"));
+        List<History> history = historyRepository.findAllByIdUser(id).orElse(null);
+        assert history != null;
+        history.sort((h1, h2) -> h2.getDate().compareTo(h1.getDate()));
+
+        return history;
     }
 
     @Transactional
-    public History save(History history){
+    public History save(UUID userId, UUID contentId){
+        historyRepository
+                .findByIdUserAndContentId(userId, contentId)
+                .ifPresent(h -> historyRepository.deleteById(h.getId()));
+
+        History history = History.builder()
+                .idUser(userId)
+                .content(contentService.findById(contentId))
+                .date(new Date())
+                .build();
+
         return historyRepository.save(history);
     }
 
