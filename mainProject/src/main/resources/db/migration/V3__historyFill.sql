fdf6a80414542748efd1a1ea8e0dee260f5b7982
leonid <leo_lis@mail.ru>
2020-06-13 17:21:13 +0300

    T-P7_views_history Добавил поля в таблицы Users и History

---
+++ mainProject/src/main/resources/db/migration/V3__historyFill.sql
@@ -0,0 +1,7 @@
+INSERT INTO tbl_user (id_user, login_user, password_user, name_user, email_user) VALUES ('b774430e-46a4-4801-8eb1-49b4022c745c', 'user1', '$2y$12$WJKnpmcShN9GxXJ/.VpZQOcZ19/.ynhJauJ159OHIOrEKvPDnFhbC', 'user1', null);
+
+INSERT INTO tbl_history (id_history, id_user, id_content, date_history) VALUES ('05373469-4fe9-48a0-9c0c-0c7d020aa952', 'ef39e14f-8e9e-4bd6-894f-3b7e99cf8089', '51bf778e-46e5-4f01-8c31-e6bb0de53a7c', '2020-06-19');
+INSERT INTO tbl_history (id_history, id_user, id_content, date_history) VALUES ('378867b0-5c73-48ac-bb28-2f0a8d46e664', 'ef39e14f-8e9e-4bd6-894f-3b7e99cf8089', '68c2769d-2e9f-4dd7-b322-1d5124a05bef', '2020-06-25');
+INSERT INTO tbl_history (id_history, id_user, id_content, date_history) VALUES ('502c1674-289e-4faa-a1d1-fe7be1a2e101', 'b774430e-46a4-4801-8eb1-49b4022c745c', '86a38fc4-a9a6-45e8-a6c8-08aac7949f25', '2020-06-01');
+INSERT INTO tbl_history (id_history, id_user, id_content, date_history) VALUES ('a0c5ef97-8063-426f-a36c-cf8679ab27e4', 'b774430e-46a4-4801-8eb1-49b4022c745c', '1c6b8365-7b92-4772-9406-458ca0e7f4ab', '2020-06-27');
+INSERT INTO tbl_history (id_history, id_user, id_content, date_history) VALUES ('8242b1f8-c864-46ed-b0c5-1ed761662a51', 'b774430e-46a4-4801-8eb1-49b4022c745c', '47725759-864e-4d4d-a601-dd52d1506e2a', '2020-06-04');
