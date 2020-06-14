606b8561e91efb4cb246b173b78799b4678d175f
svyatoslavn1000 <s-e-ny@yandex.ru>
2020-06-14 17:26:15 +0300

    Добавил расширение репозитория

--- mainProject/src/main/java/ru/gbjava/kinozen/persistence/repositories/ContentRepository.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/persistence/repositories/ContentRepository.java
@@ -1,13 +1,14 @@
 package ru.gbjava.kinozen.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.lang.NonNull;
 import ru.gbjava.kinozen.persistence.entities.Content;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ContentRepository extends JpaRepository<Content, UUID> {
+public interface ContentRepository extends JpaRepository<Content, UUID>, JpaSpecificationExecutor<Content> {
 
     Optional<Content> findContentByUrl(@NonNull String url);
 }
