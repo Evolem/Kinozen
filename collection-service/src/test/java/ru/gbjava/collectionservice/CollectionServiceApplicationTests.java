c885547c1a711c033ec098e2b3dd356a726c36ee
Milkky <Milkky598@yandex.ru>
2020-06-15 18:08:47 +0500

    T-P4 Система коллекций Добавлен новый сервис

---
+++ collection-service/src/test/java/ru/gbjava/collectionservice/CollectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gbjava.collectionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
