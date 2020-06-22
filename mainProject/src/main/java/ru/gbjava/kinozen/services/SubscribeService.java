790bc211942a73ce92a1c604c5221dfb29209b82
leonid <leo_lis@mail.ru>
2020-06-22 21:43:53 +0300

    A-X5_subscribes добавил методы: - получения списка фильмов по подписке на жанры и актеров - подписки на актеров - подписки на жанры

--- mainProject/src/main/java/ru/gbjava/kinozen/services/SubscribeService.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/services/SubscribeService.java
@@ -7,6 +7,7 @@ import ru.gbjava.kinozen.persistence.entities.Content;
 import ru.gbjava.kinozen.persistence.entities.Genre;
 import ru.gbjava.kinozen.persistence.entities.User;
 
+import javax.transaction.Transactional;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,10 +16,10 @@ import java.util.Set;
 public class SubscribeService {
 
     private final ContentService contentService;
+    private final UserService userService;
 
     public Set<Content> getContentSubscribeList (User user){
         Set<Content> contentSubscribeList = new HashSet<>();
-
         for(Content newContent: contentService.getNewContents()){
             for(Genre newContentGenre: newContent.getGenres()){
                 for(Genre subscribeGenre: user.getGenreSubscribeList()){
@@ -27,7 +28,6 @@ public class SubscribeService {
                     }
                 }
             }
-
             for(Actor newContentActor: newContent.getActors()){
                 for(Actor subscribeActor: user.getActorSubscribeList()){
                     if(newContentActor.equals(subscribeActor)){
@@ -36,8 +36,33 @@ public class SubscribeService {
                 }
             }
         }
-
         return contentSubscribeList;
+    }
+
+    @Transactional
+    public void subscribeUserToGenre(User user, Genre genre){
+        Set<Genre> userGenreSubscribeList = user.getGenreSubscribeList();
+
+        if (userGenreSubscribeList.contains(genre)) {
+            userGenreSubscribeList.remove(genre);
+        } else {
+            userGenreSubscribeList.add(genre);
+        }
+        user.setGenreSubscribeList(userGenreSubscribeList);
+        userService.save(user);
+    }
+
+    @Transactional
+    public void subscribeUserToActor(User user, Actor actor){
+        Set<Actor> userActorSubscribeList = user.getActorSubscribeList();
+
+        if (userActorSubscribeList.contains(actor)) {
+            userActorSubscribeList.remove(actor);
+        } else {
+            userActorSubscribeList.add(actor);
+        }
+        user.setActorSubscribeList(userActorSubscribeList);
+        userService.save(user);
     }
 
 }
