74d5e4153ad1efae0995731dec1502d70a2b4dcc
leonid <leo_lis@mail.ru>
2020-06-22 21:09:14 +0300

    A-X5_subscribes добавил метод получения новых фильмов

--- mainProject/src/main/java/ru/gbjava/kinozen/persistence/repositories/ContentRepository.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/persistence/repositories/ContentRepository.java
@@ -1,13 +1,21 @@
 package ru.gbjava.kinozen.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import ru.gbjava.kinozen.persistence.entities.Content;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 public interface ContentRepository extends JpaRepository<Content, UUID> {
 
+    @Query(value = "SELECT * FROM tbl_content WHERE released_content >= CURRENT_DATE - INTEGER '7'"
+            , nativeQuery = true)
+    Set<Content> getNewContents();
+
     Optional<Content> findContentByUrl(@NonNull String url);
+
 }
