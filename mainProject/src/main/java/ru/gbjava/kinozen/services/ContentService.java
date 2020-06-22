492af07214939d78880276b86eedb7ac005f9945
leonid <leo_lis@mail.ru>
2020-06-22 21:08:30 +0300

    A-X5_subscribes добавил метод получения новых фильмов

--- mainProject/src/main/java/ru/gbjava/kinozen/services/ContentService.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/services/ContentService.java
@@ -119,4 +119,8 @@ public class ContentService implements CrudService<Content, UUID> {
 
 
     }
+
+    public Set<Content> getNewContents(){
+        return contentRepository.getNewContents();
+    }
 }
