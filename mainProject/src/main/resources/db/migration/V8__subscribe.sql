b92b10736bce0e28d15a2fe19d51b45b251e8dd3
leonid <leo_lis@mail.ru>
2020-06-22 22:17:04 +0300

    fixup! A-X5_subscribes добавил таблицы подписок на жанры и актеров

---
+++ mainProject/src/main/resources/db/migration/V8__subscribe.sql
@@ -0,0 +1,20 @@
+create table if not exists tbl_subscribe_actor
+(
+    id_user uuid not null
+        constraint user___fk
+            references tbl_user,
+    id_actor uuid not null
+        constraint actor___fk
+            references tbl_actor
+);
+
+create table if not exists tbl_subscribe_genre
+(
+    id_user uuid not null
+        constraint user___fk
+            references tbl_user,
+    id_genre uuid not null
+        constraint genre___fk
+            references tbl_genre
+);
+
