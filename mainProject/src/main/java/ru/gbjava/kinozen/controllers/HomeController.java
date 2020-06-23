d3ed60550742f1d76742aa4e11abe2cd2792f4e1
leonid <leo_lis@mail.ru>
2020-06-23 20:10:58 +0300

    fixup! A-X5_subscribes добавил вывод для авторизованных юзеров списка новинок по подписке на фронт

--- mainProject/src/main/java/ru/gbjava/kinozen/controllers/HomeController.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/controllers/HomeController.java
@@ -6,17 +6,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.gbjava.kinozen.dto.ContentDto;
 import ru.gbjava.kinozen.dto.mappers.ContentMapper;
+import ru.gbjava.kinozen.services.SubscribeService;
 import ru.gbjava.kinozen.services.facade.ContentFacade;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
     private final ContentFacade contentFacade;
+    private final SubscribeService subscribeService;
+
     @GetMapping
-    public String index(Model model){
+    public String index(Model model, Principal principal){
         Iterable<ContentDto> dtoList = ContentMapper.INSTANCE.toDtoList(contentFacade.findAllContent());
         model.addAttribute("contentList", dtoList);
+        if (principal != null){
+            model.addAttribute("news", subscribeService.getContentSubscribeList(principal.getName()));
+        }
         return "index";
     }
 }
