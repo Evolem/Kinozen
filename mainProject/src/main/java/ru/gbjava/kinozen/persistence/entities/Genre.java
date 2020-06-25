60cb2a4141186bf8dd84a374b744eee7a3ade6e0
leonid <leo_lis@mail.ru>
2020-06-25 22:33:34 +0300

    A-X5_subscribes добавил поле с подписчиками

--- mainProject/src/main/java/ru/gbjava/kinozen/persistence/entities/Genre.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/persistence/entities/Genre.java
@@ -34,6 +34,9 @@ public class Genre {
             inverseJoinColumns = @JoinColumn(name = "id_content"))
     private Set<Content> contents;
 
+    @ManyToMany(mappedBy = "genreSubscribeList")
+    Set<User> genreSubscribers;
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
