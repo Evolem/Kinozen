a31f172b36ab583a44f2ace9b8035afd477296d8
Евгений <evem-w@outlook.com>
2020-06-27 02:40:42 +0300

    T-P8V2_wishlist Обновление главной страницы

--- mainProject/src/main/java/ru/gbjava/kinozen/dto/WishDto.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/dto/WishDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import ru.gbjava.kinozen.dto.ContentDto;
 
 @Getter
 @Setter
