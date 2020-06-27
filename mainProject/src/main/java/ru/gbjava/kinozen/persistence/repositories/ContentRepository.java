ada4b7441657e021a9a5a44db06659a573d7836c
leonid <leo_lis@mail.ru>
2020-06-27 20:16:43 +0300

    A-X5_subscribes добавил метод получения новых фильмов

--- mainProject/src/main/java/ru/gbjava/kinozen/persistence/repositories/ContentRepository.java
+++ mainProject/src/main/java/ru/gbjava/kinozen/persistence/repositories/ContentRepository.java
@@ -2,7 +2,6 @@ package ru.gbjava.kinozen.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import ru.gbjava.kinozen.persistence.entities.Content;
 
